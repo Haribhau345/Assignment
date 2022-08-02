76ede884e84b137dcc94f4cf26388ffbdde129be
Haribhau345 <109912644+Haribhau345@users.noreply.github.com>
2022-08-02 14:56:26 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+bacon = 22
+
+
+# In[2]:
+
+
+bacon + 1
+
+
+# In[3]:
+
+
+'spam' + 'spamspam'
+
+
+# In[4]:
+
+
+'spam' * 3
+
+
+# In[5]:
+
+
+'I have eaten ' + 99 + ' burritos.'
+
+
+# In[ ]:
+
+
+
+
